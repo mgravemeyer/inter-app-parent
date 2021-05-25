eb1839444140bd2e0aeb1465f0a03aebada2b85d
mgravemeyer <m.gravemeyer@icloud.com>
2021-05-25 17:19:03 +0200

    added methods to use UIPasteboard.general

--- inter-app-parent/ContentView.swift
+++ inter-app-parent/ContentView.swift
@@ -10,15 +10,15 @@ import MobileCoreServices
 
 struct ContentView: View {
     
-    let pasteboard = UIPasteboard(name: UIPasteboard.Name(rawValue: "com.maximiliangravemeyer.inter-app-parent.pasteboard"), create: true)
+    let pasteboard = UIPasteboard.general
     
     var body: some View {
         VStack {
-            Button("Pasteboard -> hello string!") {
-                pasteboard!.setValue("hello string!", forPasteboardType: kUTTypePlainText as String)
+            Button("Pasteboard -> [NSArray]") {
+                pasteboard.string = "String form parent"
             }
-            Button("Pasteboard -> form parent app!!") {
-                pasteboard!.setValue("from parent app!", forPasteboardType: kUTTypePlainText as String)
+            Button("Pasteboard -> UIImage") {
+                pasteboard.string = "from parent app!"
             }
         }
     }
